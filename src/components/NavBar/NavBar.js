bc1dee75acb700c232c14caa1b0c154f029d741e
Fabio <fabiodesenvolvedorpro@gmail.com>
2023-06-12 16:43:43 -0300

    feat: new image pokemon-logo-8

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,10 +10,10 @@ function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+              <a className="nav-link active" aria-current="page" href="#">Página Inicial</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#">Pokédex</a>
             </li>
           </ul>
           <form className="d-flex">
