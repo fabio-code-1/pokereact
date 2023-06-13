e0d3970966febe0f25a7e2f7b88853f649ebb8f9
Fabio <fabiodesenvolvedorpro@gmail.com>
2023-06-13 13:50:45 -0300

    style: reponsive mobile for  navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,46 +9,59 @@ function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand d-flex align-items-center justify-content-center text-center justify-content-sm-center" href="#home">
+        <a className="navbar-brand d-flex align-items-center justify-content-center text-center justify-content-sm-center" href="/">
           <img src={logo} className="img-fluid sm-img" alt="logo-pokemon" width="300" height="100" />
         </a>
-        <button className="navbar-toggler mx-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button className="navbar-toggler mx-auto button-bnt" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 w-100 align-items-center justify-content-center">
-            <li className="nav-item">
-              <a className="nav-link d-flex flex-column align-items-center" href="#home">
-                <img src={pikachu} className="img-fluid mb-2" alt="icon" width="50" height="40" />
-                <span className="text-center btn btn-warning text-light btn-sm">Início</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex flex-column align-items-center" href="#home">
-                <img src={pokedex} className="img-fluid mb-2" alt="icon" width="50" height="40" />
-                <span className="text-center btn btn-dark text-light btn-sm">Pokédex</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex flex-column align-items-center" href="#home">
-                <img src={bullbasaur} className="img-fluid mb-2" alt="icon" width="50" height="40" />
-                <span className="text-center btn btn-success text-light btn-sm">Link</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex flex-column align-items-center" href="#home">
-                <img src={charmander} className="img-fluid mb-2" alt="icon" width="50" height="40" />
-                <span className="text-center btn btn-danger text-light btn-sm">Link</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link d-flex flex-column align-items-center" href="#home">
-                <img src={squirtle} className="img-fluid mb-2" alt="icon" width="50" height="40" />
-                <span className="text-center btn btn-primary text-light btn-sm">Link</span>
-              </a>
-            </li>
+            <div className="d-flex flex-wrap">
+              <div className="nav-item ms-3">
+                <a className="nav-link d-flex flex-column align-items-center" href="#home">
+                  <img src={pikachu} className="img-fluid mb-2" alt="icon" width="50" height="40" />
+                  <span className="text-center btn btn-warning text-light btn-sm">Início</span>
+                </a>
+              </div>
+              <div className="nav-item ms-3">
+                <a className="nav-link d-flex flex-column align-items-center" href="#home">
+                  <img src={pokedex} className="img-fluid mb-2" alt="icon" width="50" height="40" />
+                  <span className="text-center btn btn-dark text-light btn-sm">Pokédex</span>
+                </a>
+              </div>
+              <div className="nav-item ms-3">
+                <a className="nav-link d-flex flex-column align-items-center" href="#home">
+                  <img src={bullbasaur} className="img-fluid mb-2" alt="icon" width="50" height="40" />
+                  <span className="text-center btn btn-success text-light btn-sm">Link</span>
+                </a>
+              </div>
+            </div>
+
+            <div className="d-flex flex-wrap">
+              <div className="nav-item ms-3">
+                <a className="nav-link d-flex flex-column align-items-center" href="#home">
+                  <img src={charmander} className="img-fluid mb-2" alt="icon" width="50" height="40" />
+                  <span className="text-center btn btn-danger text-light btn-sm">Link</span>
+                </a>
+              </div>
+              <div className="nav-item ms-3">
+                <a className="nav-link d-flex flex-column align-items-center" href="#home">
+                  <img src={squirtle} className="img-fluid mb-2" alt="icon" width="50" height="40" />
+                  <span className="text-center btn btn-primary text-light btn-sm">Link</span>
+                </a>
+              </div>
+              <div className="nav-item ms-3">
+                <div className="nav-link d-flex flex-column align-items-center" href="#home">
+                  <img src={squirtle} className="img-fluid mb-2" alt="icon" width="50" height="40" />
+                  <span className="text-center text-dark btn-sm">Light</span>
+                </div>
+              </div>
+            </div>
           </ul>
+
+
           <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Pokemon... " aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Pesquisar</button>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './styles/global.css';
 import App from './pages/home/App.js';
 import Header from './components/header/Header.js';
 import Footer from './components/footer/Footer.js';
